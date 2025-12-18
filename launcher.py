8d2ea13b937d28a67590721a36c7a536373cfdc0
mathis <mathis.deronne@gmail.com>
2025-12-18 14:26:55 +0100

    launcher added

---
+++ launcher.py
@@ -0,0 +1,13 @@
+import subprocess
+
+commands = [
+    r'.\venv\Scripts\python.exe -m chronobio.game.server -p 12345',
+    r'.\venv\Scripts\python.exe -m chronobio.viewer -p 12345',
+    r'.\venv\Scripts\python.exe -m local_base -p 12345 -u "Les potes âgés"'
+]
+
+for cmd in commands:
+    subprocess.Popen(
+        f'start cmd /k "{cmd}"',
+        shell=True
+    )
