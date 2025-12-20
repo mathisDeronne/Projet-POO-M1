4aa8bb8c9e2cc16f4ed7abf9a39cef9a733ce6ff
Guillaume045 <lanfroidguillaume@gmail.com>
2025-12-20 22:05:51 +0100

    nouveaux test 54%

--- src/farm/soup_factory.py
+++ src/farm/soup_factory.py
@@ -1,34 +1,4 @@
-class Stock:
-    def __init__(self, data: dict):
-        self.potato = data["POTATO"]
-        self.leek = data["LEEK"]
-        self.tomato = data["TOMATO"]
-        self.onion = data["ONION"]
-        self.zucchini = data["ZUCCHINI"]
-
-    def have_potato(self) -> bool:
-        return self.potato > 0
-
-    def have_leek(self) -> bool:
-        return self.leek > 0
-
-    def have_tomato(self) -> bool:
-        return self.tomato > 0
-
-    def have_onion(self) -> bool:
-        return self.onion > 0
-
-    def have_zucchini(self) -> bool:
-        return self.zucchini > 0
-
-    def have_stocks(self) -> bool:
-        return (
-            self.have_potato()
-            or self.have_leek()
-            or self.have_tomato()
-            or self.have_onion()
-            or self.have_zucchini()
-        )
+from src.farm.stock_factory import Stock
 
 
 class SoupFactory:
@@ -37,7 +7,10 @@ class SoupFactory:
         self.stock = Stock(data["stock"])
 
     def is_open(self) -> bool:
-        return self.days_off == 0
+        if not isinstance(self.days_off, int):
+            return False
+        else:
+            return self.days_off == 0
 
     def has_stock(self) -> bool:
         return self.stock.have_stocks()
--- tests/test_soup_factory.py
+++ tests/test_soup_factory.py
@@ -1,22 +1,69 @@
-from src.farm.soup_factory import SoupFactory, Stock
+from pytest import fixture, mark
+from src.farm.soup_factory import SoupFactory
 
 
-def test_stock():
-    stock = Stock({"POTATO": 0, "LEEK": 0, "TOMATO": 0, "ONION": 0, "ZUCCHINI": 0})
-    assert stock.have_potato() is False
-    assert stock.have_leek() is False
-    assert stock.have_tomato() is False
-    assert stock.have_onion() is False
-    assert stock.have_zucchini() is False
-    assert stock.have_stocks() is False
+@fixture
+def base_soup_factory_data():
+    return {
+        "days_off": 0,
+        "stock": {
+            "POTATO": 0,
+            "LEEK": 2000,
+            "TOMATO": 0,
+            "ONION": 2000,
+            "ZUCCHINI": 0,
+        },
+    }
 
 
-def test_Soup_factory():
-    soup_factory = SoupFactory(
-        {
-            "days_off": 0,
-            "stock": {"POTATO": 0, "LEEK": 0, "TOMATO": 0, "ONION": 0, "ZUCCHINI": 0},
-        }
-    )
+def test_base_soup_factory(base_soup_factory_data):
+    soup_factory = SoupFactory(base_soup_factory_data)
     assert soup_factory.is_open() is True
-    assert soup_factory.has_stock() is False
+    assert soup_factory.has_stock() is True
+
+
+@mark.parametrize(
+    "value, expected",
+    [
+        (0, True),
+        (-0, True),
+        (+0, True),
+        (5, False),
+        ("0", False),
+        (0.0, False),
+        (+0.0, False),
+        (-0.0, False),
+    ],
+)
+def test_days_off(base_soup_factory_data, value, expected):
+    base_soup_factory_data["days_off"] = value
+    soup_factory = SoupFactory(base_soup_factory_data)
+    assert soup_factory.is_open() is expected
+
+
+@mark.parametrize(
+    "vegetable, value, expected",
+    [
+        ("POTATO", 2000, True),
+        ("LEEK", 100, True),
+        ("TOMATO", 300, True),
+        ("ONION", 100, True),
+        ("ZUCCHINI", 5, True),
+        ("POTATO", 0, False),
+        ("LEEK", 0, False),
+        ("TOMATO", -500, False),
+        ("ONION", 0, False),
+        ("ZUCCHINI", "test", False),
+        ("POTATO", "2000", False),
+        ("TOMATO", 400.0, False),
+        ("ONION", +1000.0, False),
+        ("ZUCCHINI", -200.0, False),
+    ],
+)
+def test_have_stock(base_soup_factory_data, vegetable, value, expected):
+    for vegetables in base_soup_factory_data["stock"]:
+        base_soup_factory_data["stock"][vegetables] = 0
+
+    base_soup_factory_data["stock"][vegetable] = value
+    soup_factory = SoupFactory(base_soup_factory_data)
+    assert soup_factory.has_stock() is expected
