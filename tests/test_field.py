cb1c405d41b0f557e61024e1a641736c82611b1a
Guillaume045 <lanfroidguillaume@gmail.com>
2025-12-20 18:33:30 +0100

    on passe de 33% à 60% testé

--- tests/test_field.py
+++ tests/test_field.py
@@ -9,3 +9,21 @@ def test_field():
     assert field.is_bought() is True
     assert field.is_needed_water() is True
     assert field.is_location() is True
+
+
+def test_field2():
+    field = Field(
+        {"content": "POTATO", "needed_water": 0, "bought": False, "location": "test"}
+    )
+    assert field.is_content_empty() is False
+    assert field.is_bought() is False
+    assert field.is_needed_water() is False
+    assert field.is_location() is False
+
+
+def test_field3():
+    field = Field(
+        {"content": "POTATO", "needed_water": -1, "bought": False, "location": "FIELD7"}
+    )
+    assert field.is_needed_water() is False
+    assert field.is_location() is False
