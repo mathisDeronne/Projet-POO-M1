264907fe2e2dc7f65bfb6b522b03b1bf13481d38
Guillaume045 <lanfroidguillaume@gmail.com>
2025-12-20 12:26:44 +0100

    premier test de field

--- src/farm/field.py
+++ src/farm/field.py
@@ -1,3 +1,24 @@
 class Field:
-    def __init__(self, size: int):
-        pass
+    def __init__(self, data: int):
+        self.content = data["content"]
+        self.needed_water = data["needed_water"]
+        self.bought = data["bought"]
+        self.location = data["location"]
+
+    def is_content_empty(self) -> bool:
+        return self.content == "NONE"
+
+    def is_bought(self) -> bool:
+        return self.bought is True
+
+    def is_needed_water(self) -> bool:
+        return self.needed_water > 0
+
+    def is_location(self) -> bool:
+        if not self.location.startswith("FIELD"):
+            return False
+        try:
+            num = int(self.location.replace("FIELD", ""))
+        except ValueError:
+            return False
+        return 1 <= num <= 5
--- tests/test_field.py
+++ tests/test_field.py
@@ -1,5 +1,11 @@
 from src.farm.field import Field
 
 
-def test_is_sow():
-    pass
+def test_field():
+    field = Field(
+        {"content": "NONE", "needed_water": 6, "bought": True, "location": "FIELD2"}
+    )
+    assert field.is_content_empty() is True
+    assert field.is_bought() is True
+    assert field.is_needed_water() is True
+    assert field.is_location() is True
