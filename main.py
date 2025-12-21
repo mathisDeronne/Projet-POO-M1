1ce8a6a6ceb58936d0c8168e3e380315deea5675
Guillaume045 <lanfroidguillaume@gmail.com>
2025-12-21 13:59:23 +0100

    modification des stratégie pour pouvoir test

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from src.stratégie.leo_base import main as run_client
+from src.stratégie.base import main as run_client
+from src.stratégie.damien_bloucle2 import strategy as strategy_used
 
 if __name__ == "__main__":
-    run_client(username="Les potes âgés", address="127.0.0.1", port=12345)
+    run_client(
+        username="Les potes âgés",
+        address="127.0.0.1",
+        port=12345,
+        strategy=strategy_used,
+    )
--- src/stratégie/base.py
+++ src/stratégie/base.py
@@ -1,16 +1,29 @@
 import argparse
-import json
 from typing import NoReturn
 
 from chronobio.network.client import Client
 
 
+def strategy_used(client: "PlayerGameClient", game_data: dict, my_farm: dict) -> None:
+    if game_data["day"] == 0:
+        client.add_command("0 EMPRUNTER 100000")
+        client.add_command("0 ACHETER_CHAMP")
+        client.add_command("0 ACHETER_CHAMP")
+        client.add_command("0 ACHETER_CHAMP")
+        client.add_command("0 ACHETER_TRACTEUR")
+        client.add_command("0 ACHETER_TRACTEUR")
+        client.add_command("0 EMPLOYER")
+        client.add_command("0 EMPLOYER")
+        client.add_command("1 SEMER PATATE 3")
+
+
 class PlayerGameClient(Client):
     def __init__(
-        self: "PlayerGameClient", server_addr: str, port: int, username: str
+        self: "PlayerGameClient", server_addr: str, port: int, username: str, strategy
     ) -> None:
         super().__init__(server_addr, port, username, spectator=False)
         self._commands: list[str] = []
+        self.strategy = strategy
         self.vente1 = 0
 
     def run(self: "PlayerGameClient") -> NoReturn:
@@ -24,23 +37,7 @@ class PlayerGameClient(Client):
                 raise ValueError(f"My farm is not found ({self.username})")
             print(my_farm)
 
-            with open("my_farm.txt", "w", encoding="utf-8") as f:
-                f.write(json.dumps(my_farm, indent=4, ensure_ascii=False))
-
-            if game_data["day"] == 0:
-                self.add_command("0 EMPRUNTER 100000")
-                self.add_command("0 ACHETER_CHAMP")
-                self.add_command("0 EMPLOYER")
-                self.add_command("1 SEMER PATATE 1")
-            elif my_farm["fields"][0]["content"] != "NONE":
-                if my_farm["fields"][0]["needed_water"] > 0:
-                    self.add_command("1 ARROSER 1")
-                elif game_data["day"] >= self.vente1:
-                    self.add_command("0 VENDRE 1")
-                    self.vente1 = game_data["day"] + 2
-            elif game_data["day"] >= self.vente1:
-                self.add_command("1 SEMER PATATE 1")
-
+            self.strategy(self, game_data, my_farm)
             self.send_commands()
 
     def add_command(self: "PlayerGameClient", command: str) -> None:
@@ -53,8 +50,10 @@ class PlayerGameClient(Client):
         self._commands.clear()
 
 
-def main(username: str, address: str = "localhost", port: int = 16210) -> None:
-    client = PlayerGameClient(address, port, username)
+def main(
+    username: str, address: str = "localhost", port: int = 16210, strategy=strategy_used
+):
+    client = PlayerGameClient(address, port, username, strategy)
     client.run()
 
 
@@ -64,18 +63,22 @@ if __name__ == "__main__":
         "-a",
         "--address",
         type=str,
+        help="name of server on the network",
         default="localhost",
     )
     parser.add_argument(
         "-p",
         "--port",
         type=int,
+        help="location where server listens",
         default=16210,
     )
     parser.add_argument(
         "-u",
         "--username",
         type=str,
+        help="name of the user",
+        default="unknown",
         required=True,
     )
     args = parser.parse_args()
