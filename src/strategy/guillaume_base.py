af08a437e92138ed79c7329b64faf3429f338d9b
Guillaume045 <lanfroidguillaume@gmail.com>
2026-01-02 13:17:36 +0100

    base logique 1

--- src/strategy/guillaume_base.py
+++ src/strategy/guillaume_base.py
@@ -29,3 +29,106 @@ def strategy(client, game_data: dict, my_farm: dict):
     # les 5 emplyer arrose
     # je vend le champs 1
     ## ...
+
+    if day == 0:
+        for _ in range(2):
+            client.add_command("0 ACHETER_CHAMP")
+        for _ in range(11):
+            client.add_command("0 EMPLOYER")
+        client.add_command("1 SEMER PATATE 1")
+        client.add_command("2 ARROSER 1")
+        client.add_command("3 ARROSER 1")
+        client.add_command("4 ARROSER 1")
+        client.add_command("5 ARROSER 1")
+        client.add_command("6 ARROSER 1")
+        client.add_command("7 ARROSER 1")
+        client.add_command("8 ARROSER 1")
+        client.add_command("9 ARROSER 1")
+        client.add_command("10 ARROSER 1")
+        client.add_command("11 ARROSER 1")
+    if day == 1:
+        client.add_command("0 VENDRE 1")
+    if day == 2:
+        client.add_command("1 SEMER PATATE 2")
+        client.add_command("2 ARROSER 2")
+        client.add_command("3 ARROSER 2")
+        client.add_command("4 ARROSER 2")
+        client.add_command("5 ARROSER 2")
+        client.add_command("6 ARROSER 2")
+        client.add_command("7 ARROSER 2")
+        client.add_command("8 ARROSER 2")
+        client.add_command("9 ARROSER 2")
+        client.add_command("10 ARROSER 2")
+        client.add_command("11 ARROSER 2")
+    if day == 3:
+        client.add_command("0 VENDRE 2")
+    if day == 4:
+        client.add_command("1 SEMER PATATE 1")
+        client.add_command("2 ARROSER 1")
+        client.add_command("3 ARROSER 1")
+        client.add_command("4 ARROSER 1")
+        client.add_command("5 ARROSER 1")
+        client.add_command("6 ARROSER 1")
+        client.add_command("7 ARROSER 1")
+        client.add_command("8 ARROSER 1")
+        client.add_command("9 ARROSER 1")
+        client.add_command("10 ARROSER 1")
+        client.add_command("11 ARROSER 1")
+    if day == 5:
+        client.add_command("0 VENDRE 1")
+    if day == 6:
+        client.add_command("1 SEMER PATATE 2")
+        client.add_command("2 ARROSER 2")
+        client.add_command("3 ARROSER 2")
+        client.add_command("4 ARROSER 2")
+        client.add_command("5 ARROSER 2")
+        client.add_command("6 ARROSER 2")
+        client.add_command("7 ARROSER 2")
+        client.add_command("8 ARROSER 2")
+        client.add_command("9 ARROSER 2")
+        client.add_command("10 ARROSER 2")
+        client.add_command("11 ARROSER 2")
+    if day == 7:
+        client.add_command("0 VENDRE 2")
+    if day == 8:
+        client.add_command("1 SEMER PATATE 1")
+        client.add_command("2 ARROSER 1")
+        client.add_command("3 ARROSER 1")
+        client.add_command("4 ARROSER 1")
+        client.add_command("5 ARROSER 1")
+        client.add_command("6 ARROSER 1")
+        client.add_command("7 ARROSER 1")
+        client.add_command("8 ARROSER 1")
+        client.add_command("9 ARROSER 1")
+        client.add_command("10 ARROSER 1")
+        client.add_command("11 ARROSER 1")
+    if day == 9:
+        client.add_command("0 VENDRE 1")
+    if day == 10:
+        client.add_command("1 SEMER PATATE 2")
+        client.add_command("2 ARROSER 2")
+        client.add_command("3 ARROSER 2")
+        client.add_command("4 ARROSER 2")
+        client.add_command("5 ARROSER 2")
+        client.add_command("6 ARROSER 2")
+        client.add_command("7 ARROSER 2")
+        client.add_command("8 ARROSER 2")
+        client.add_command("9 ARROSER 2")
+        client.add_command("10 ARROSER 2")
+        client.add_command("11 ARROSER 2")
+    if day == 11:
+        client.add_command("0 VENDRE 2")
+    if day == 12:
+        client.add_command("1 SEMER PATATE 1")
+        client.add_command("2 ARROSER 1")
+        client.add_command("3 ARROSER 1")
+        client.add_command("4 ARROSER 1")
+        client.add_command("5 ARROSER 1")
+        client.add_command("6 ARROSER 1")
+        client.add_command("7 ARROSER 1")
+        client.add_command("8 ARROSER 1")
+        client.add_command("9 ARROSER 1")
+        client.add_command("10 ARROSER 1")
+        client.add_command("11 ARROSER 1")
+    if day == 13:
+        client.add_command("0 VENDRE 1")
