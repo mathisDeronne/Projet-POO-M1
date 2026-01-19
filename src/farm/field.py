bae869c85b9faf9e9ac02929454fa2a115573568
Guillaume045 <lanfroidguillaume@gmail.com>
2026-01-19 14:54:14 +0100

    test crach

--- src/farm/field.py
+++ src/farm/field.py
@@ -15,13 +15,6 @@ class Field:
             and self.content == "NONE"
         )
 
-    def is_content_full(self) -> bool:
-        return (
-            isinstance(self.content, str)
-            and not isinstance(self.content, bool)
-            and self.content in Legume
-        )
-
     def is_bought(self) -> bool:
         return isinstance(self.bought, bool) and self.bought
 
