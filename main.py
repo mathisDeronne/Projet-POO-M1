3a25b4c7d981bc22ef3f41bf00fb17fad098fb80
Guillaume045 <lanfroidguillaume@gmail.com>
2026-01-19 09:04:43 +0100

    main --

--- main.py
+++ main.py
@@ -1,10 +1,28 @@
 from src.strategy.base import main as run_client
-from src.strategy.guillaume_base import strategy as strategy_used
+from src.strategy.damien_bloucle2 import strategy as strategy_used
+import argparse
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Game client.")
+    parser.add_argument(
+        "-a",
+        "--address",
+        type=str,
+        help="name of server on the network",
+        default="localhost",
+    )
+    parser.add_argument(
+        "-p",
+        "--port",
+        type=int,
+        help="location where server listens",
+        default=16210,
+    )
+    args = parser.parse_args()
+
     run_client(
         username="Les potes âgés",
-        address="127.0.0.1",
-        port=12345,
+        address=args.address,
+        port=args.port,
         strategy=strategy_used,
     )
